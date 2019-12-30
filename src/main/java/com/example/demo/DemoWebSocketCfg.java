c500259f39b89d8ac3ecf50344c7eea5e19e399d
zhaoyu <zhaoyu600453@pwrd.com>
2019-12-30 15:25:17 +0800

    add HandshakeInterceptor

--- src/main/java/com/example/demo/DemoWebSocketCfg.java
+++ src/main/java/com/example/demo/DemoWebSocketCfg.java
@@ -1,17 +1,29 @@
 package com.example.demo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 @Configuration
 @EnableWebSocket
 public class DemoWebSocketCfg implements WebSocketConfigurer {
 
+	static DemoWebSocketHandler handler = new DemoWebSocketHandler();
+
 	@Override
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-		registry.addHandler(new DemoWebSocketHandler(), "/wsdemo").setAllowedOrigins("*");
+		registry.addHandler(handler, "/wsdemo").setAllowedOrigins("http://127.0.0.1").addInterceptors(handler);
+	}
+
+	@Bean
+	public ServletServerContainerFactoryBean createWebSocketContainer() {
+		ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+		container.setMaxTextMessageBufferSize(8192);
+		container.setMaxBinaryMessageBufferSize(8192);
+		return container;
 	}
 
 }
--- src/main/java/com/example/demo/DemoWebSocketHandler.java
+++ src/main/java/com/example/demo/DemoWebSocketHandler.java
@@ -1,16 +1,36 @@
 package com.example.demo;
 
+import java.util.Map;
+
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
 
-public class DemoWebSocketHandler extends TextWebSocketHandler {
+public class DemoWebSocketHandler extends TextWebSocketHandler implements HandshakeInterceptor {
+
+	@Override
+	public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler,
+			Map<String, Object> attributes) throws Exception {
+		System.out.println("before Handshake " + request);
+		return true;
+	}
+
+	@Override
+	public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler,
+			Exception exception) {
+		System.out.println("after Handshake");
+	}
 
 	@Override
 	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
 		String payload = message.getPayload();
 //		Map<String, String> map = JSONObject.parseObject(payload, HashMap.class);
-		System.out.println(session + " recv: " + payload);
+		System.out.println("handleTextMessage:" + session + " recv: " + payload);
 		session.sendMessage(new TextMessage("send: " + payload));
 	}
+
 }
